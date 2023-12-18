d6eddeaa1bfcd2e57732d70dd7f80b1913d9c280
gcharland1 <g.charland1@protonmail.com>
2023-12-18 07:52:47 -0500

    Center after page jump

--- lua/gcharland/remap.lua
+++ lua/gcharland/remap.lua
@@ -16,3 +16,12 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 -- Delete/Paste without overwriting register
 vim.keymap.set("v", "<leader>p", "pgvy")
 vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Center window after big jumps
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+
+
+-- Add to harpoon in Netrw
+
